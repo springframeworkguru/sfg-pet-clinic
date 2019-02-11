3c61e0ffd12753b18aa1bdc6d61ffef750424214
chongying <chongyingqiu@gmail.com>
2019-02-10 22:12:13 -0500

    updates in new branch

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SfgPetClinicApplication {
+public class SfgPetClinicApplication {  //new branch errorDisplayOwner
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
