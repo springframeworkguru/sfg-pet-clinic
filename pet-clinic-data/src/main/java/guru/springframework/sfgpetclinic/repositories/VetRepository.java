194dc3f610e1457b3ddd0e630671665f3c44c2d7
matin69 <matin3entaor0@gmail.com>
2021-10-27 14:38:44 +0330

    Add "spring-jpa" profile for Jpa repositories

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/VetRepository.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Vet;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by jt on 8/5/18.
  */
+@Profile("spring-jpa")
 public interface VetRepository extends CrudRepository<Vet, Long> {
 }
