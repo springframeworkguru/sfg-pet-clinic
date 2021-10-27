2748a44264df7e2bc32c53bf2abab361d41baa9b
matin69 <matin3entaor0@gmail.com>
2021-10-27 15:02:22 +0330

    Move Visit map service logic to new Visit map repository

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/map/VisitMapRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/map/VisitMapRepository.java
@@ -7,5 +7,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @Profile("map")
-public class VisitMapRepository extends CrudMapRepository<Visit,Long> implements VisitRepository {
+public class VisitMapRepository extends CrudMapRepository<Visit, Long> implements VisitRepository {
+
+    @Override
+    public <S extends Visit> S save(S s) {
+        if (s.getPet() == null || s.getPet().getOwner() == null || s.getPet().getId() == null
+                || s.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(s);
+    }
 }
