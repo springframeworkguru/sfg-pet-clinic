16c0330789654d18b189ad939507c8e50713d3bd
Willem_Windows <wimjoost@gmail.com>
2023-02-14 14:43:01 +0100

    implement BaseEntity functionality

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.model;
+
+import java.io.Serializable;
+
+/**
+ * @author Hoek0024 on 14 feb. 2023
+ *
+ */
+public class BaseEntity implements Serializable {
+	
+	private Long id;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 /**
  * Created by jt on 7/13/18.
  */
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
