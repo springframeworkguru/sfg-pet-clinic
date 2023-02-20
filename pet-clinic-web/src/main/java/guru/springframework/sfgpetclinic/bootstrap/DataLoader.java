7db1b00c82960a25350ecbe264f3d1ef1b1cdde4
Willem_Windows <wimjoost@gmail.com>
2023-02-20 13:07:16 +0100

    list vets

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -63,7 +63,7 @@ public class DataLoader implements CommandLineRunner {
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
-        vetService.save(vet1);
+        vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
 	}
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,18 +3,37 @@
  */
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.interfaces.VetService;
 
 /**
  * @author Hoek0024 on 15 feb. 2023
  *
  */
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-	@GetMapping({ "/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	private final VetService vetService;
+
+	/**
+	 * @param vetService
+	 */
+	@Autowired
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@GetMapping({ "", "/", "/index", "/index.html" })
+	public String listVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 
