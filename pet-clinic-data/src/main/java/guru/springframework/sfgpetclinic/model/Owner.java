8591d9020cf4143bf3c0cbce4c15123f3fcab106
Willem_Windows <wimjoost@gmail.com>
2023-02-23 10:12:54 +0100

    add contact info to owner

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -7,7 +7,52 @@ import java.util.Set;
  */
 public class Owner extends Person {
 
+	private String address;
+	private String city;
+	private String telephone;
 	private Set<Pet> pets;
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return this.address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return this.city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return this.telephone;
+	}
+
+	/**
+	 * @param telephone the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 	/**
 	 * @return the pets
