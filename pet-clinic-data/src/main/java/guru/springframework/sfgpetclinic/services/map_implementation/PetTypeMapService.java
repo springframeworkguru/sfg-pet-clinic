3f1ada550c749b2bade193a4a5f3e684c682417f
Willem_Windows <wimjoost@gmail.com>
2023-02-23 11:36:15 +0100

    add service annotation to PetTypeMapService.java

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map_implementation/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map_implementation/PetTypeMapService.java
@@ -3,6 +3,8 @@
  */
 package guru.springframework.sfgpetclinic.services.map_implementation;
 
+import org.springframework.stereotype.Service;
+
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.interfaces.PetTypeService;
 
@@ -10,6 +12,7 @@ import guru.springframework.sfgpetclinic.services.interfaces.PetTypeService;
  * @author Hoek0024 on 23 feb. 2023
  *
  */
+@Service
 public class PetTypeMapService extends AbstractBaseEntityMapService<PetType> implements PetTypeService<PetType> {
 
 }
