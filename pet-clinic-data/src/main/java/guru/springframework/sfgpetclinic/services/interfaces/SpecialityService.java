17e517cc7b5103ee8428d82fdf182c3b856db64e
Willem_Windows <wimjoost@gmail.com>
2023-02-27 14:31:57 +0100

    create speciality map service. closes #12

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/interfaces/SpecialityService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services.interfaces;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * @author Hoek0024 on 27 feb. 2023
+ *
+ */
+public interface SpecialityService extends BaseEntityService<Speciality> {
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map_implementation/SpecialityMapService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services.map_implementation;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.interfaces.SpecialityService;
+
+/**
+ * @author Hoek0024 on 27 feb. 2023
+ *
+ */
+@Service
+public class SpecialityMapService extends AbstractBaseEntityMapService<Speciality> implements SpecialityService {
+
+}
