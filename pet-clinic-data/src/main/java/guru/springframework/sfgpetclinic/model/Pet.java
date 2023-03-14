3258c43d385713bc873091dcb51cdc981d1805ab
Willem_Windows <wimjoost@gmail.com>
2023-03-14 12:04:06 +0100

    create visit entity. closes #7

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -28,6 +32,10 @@ public class Pet extends BaseEntity {
     
     @Column(name = "birth_date")
     private LocalDate birthDate;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+    
 	/**
 	 * @return the name
 	 */
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -5,14 +5,28 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * @author Hoek0024 on 23 feb. 2023
  *
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+	@Column(name = "date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+    @JoinColumn(name = "pet_id")
 	private Pet pet;
 
 	/**
