24be170bee34792956a5f09a40847a9ec35fd359
Willem_Windows <wimjoost@gmail.com>
2023-03-16 13:40:12 +0100

    implement findByFirstName and findByLastName in personMapService

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map_implementation/PersonMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map_implementation/PersonMapService.java
@@ -10,19 +10,18 @@ import guru.springframework.sfgpetclinic.services.interfaces.PersonService;
  * @author Hoek0024 on 20 feb. 2023
  *
  */
-public abstract class PersonMapService<T extends Person> extends AbstractBaseEntityMapService<T> implements PersonService<T> {
+public abstract class PersonMapService<T extends Person> extends AbstractBaseEntityMapService<T>
+		implements PersonService<T> {
 	@Override
 	public T findByLastName(String lastName) {
-		
-		// TODO Auto-generated method stub
-		return null;
+		return this.findAll().stream().filter(person -> person.getLastName().equalsIgnoreCase(lastName)).findFirst()
+				.orElse(null);
 	}
 
 	@Override
 	public T findByFirstName(String firstName) {
-		
-		// TODO Auto-generated method stub
-		return null;
+		return this.findAll().stream().filter(person -> person.getFirstName().equalsIgnoreCase(firstName)).findFirst()
+				.orElse(null);
 	}
 
 }
