9f9ac5202ee0efc714e64508c6992bd87e288755
Willem_Windows <wimjoost@gmail.com>
2023-03-16 14:33:58 +0100

    implementation of owner map service test. closes #19

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map_implementation/OwnerMapServiceTest.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package guru.springframework.sfgpetclinic.services.map_implementation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.interfaces.PetService;
+
+/**
+ * @author Hoek0024 on 16 mrt. 2023
+ *
+ */
+class OwnerMapServiceTest {
+	
+	OwnerMapService ownerMapService;
+
+	@Mock
+	PetService petService;
+	
+	@Captor
+	ArgumentCaptor<Pet> captor;
+	
+	Long id = 1L;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		
+		ownerMapService = new OwnerMapService(petService);
+	}
+
+	/**
+	 * Test method for {@link guru.springframework.sfgpetclinic.services.map_implementation.OwnerMapService#save(guru.springframework.sfgpetclinic.model.Owner)}.
+	 */
+	@Test
+	void testSaveOwner() {
+		//given
+		Pet stef = Pet.builder().name("Stef").build();
+		Set<Pet> pets = new HashSet<>();
+		pets.add(stef);
+		Owner owner = Owner.builder().id(id).pets(pets).build();
+		
+		//when
+		Owner savedOwner = ownerMapService.save(owner);
+		
+		//then
+		assertEquals(id, savedOwner.getId());
+		verify(petService, times(1)).save(captor.capture());
+		assertTrue(captor.getAllValues().get(0) == stef);
+	}
+
+}
